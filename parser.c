ce9f0f911edf6084648d9ba8542fcac94cb9507a
Dominik Klon <dklon@redhat.com>
2022-11-05 18:55:19 +0100

    commented parser code to not cause errors in make

--- parser.c
+++ parser.c
@@ -7,24 +7,24 @@
 #include "parser.h"
 
 
-err_type next_check(token_t* t){
-    if(!getNextToken(t)){
-        return ERR_LEX;
-    }
-}
+// err_type next_check(token_t* t){
+//     if(!getNextToken(t)){
+//         return ERR_LEX;
+//     }
+// }
 
-err_type next_check_type(token_type_t* token_type, token_type_t* valid_type){
-    if (token_type != valid_type){
-        return ERR_PARSE;
-    }
-}
+// err_type next_check_type(token_type_t* token_type, token_type_t* valid_type){
+//     if (token_type != valid_type){
+//         return ERR_PARSE;
+//     }
+// }
 
-void syntax_check(token_t* token){
-    switch(token->type){
-        case T_Identifier:
-            next_check(token);
-            next_check_type(token->type, T_Assign);
-        default:
-            return 0;
-    }
-}
+// void syntax_check(token_t* token){
+//     switch(token->type){
+//         case T_Identifier:
+//             next_check(token);
+//             next_check_type(token->type, T_Assign);
+//         default:
+//             return 0;
+//     }
+// }
