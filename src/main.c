6ecbcdef323f392611c59446bc9a1548fdc0eccb
Matyáš Strelec <matyasstrelec@gmail.com>
2022-11-29 21:12:58 +0100

    symtable test in main

--- src/main.c
+++ src/main.c
@@ -31,6 +31,31 @@ int main()
         printf("Lexer success\n");
     }
 
+    // Initialize symbol table
+    symtable_t *symtable = symtable_init(19);
+    symtable = symtable_check_size(symtable);
+
+    // Fill symbol table with tokens
+    for (int i = 0; tokens->activeToken->type != T_File_end; tokens->activeToken = tokens->activeToken->next)
+    {
+        if (tokens->activeToken->type == T_Identifier ||
+            tokens->activeToken->type == T_Var_id ||
+            tokens->activeToken->type == T_String ||
+            tokens->activeToken->type == T_Int ||
+            tokens->activeToken->type == T_Float)
+        {
+            symtable_insert(symtable, token_to_symbol(tokens->activeToken));
+        }
+    }
+    symtable_print(symtable);
+
+    void *aa = symtable_lookup(symtable, "$d");
+    printf("aa: %p", aa);
+
+    tokens->activeToken = tokens->firstToken;
+
+
+
     // Call parser
     error = parser(tokens);
     if (error != 0)
