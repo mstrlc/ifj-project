d2596d53c6e3ed7d4b2a0da70298d2fcd9842165
Seidly <seidlondra@gmail.com>
2022-12-02 00:00:12 +0100

    started on exp-parser that actually builds tree :)

--- src/exp_parser.c
+++ src/exp_parser.c
@@ -39,9 +39,9 @@ int precedence(token_type_t operator)
 }
 
 /**
- * @brief Builds a expresion parse tree from the given token list
- *
- *
+ * @brief Parses the expression
+ * 
+ * uses @nameparse_expression() function to build parse tree
  *
  * @param tokens list of tokens
  * @return int EXIT_SUCCESS or EXIT_FAILURE
@@ -50,12 +50,10 @@ int exp_parser(token_list_t *tokens)
 {
     int error = 0;
     PTreeNode_t *PTree = initPtree();
-    if (ACTIVE_TYPE == T_Int || ACTIVE_TYPE == T_Exp || ACTIVE_TYPE == T_Float || ACTIVE_TYPE == T_String || ACTIVE_TYPE == T_L_r_par || ACTIVE_TYPE == T_Var_id)
-    {
-        PTree = parse_expression(tokens, 1, PTree);
-        printPtree(PTree);
-        return 0;
-    }
+    PTree = parse_expression_with_tree(tokens, 1, PTree, &error);
+    printPtree(PTree);
+    return 0;
+    
 }
 
 /**
@@ -64,8 +62,8 @@ int exp_parser(token_list_t *tokens)
  * Goes through the token list and builds a parse tree from it
  *
  * @param tokens list of tokens
- * @param min_precedence minimum precedence of the operator
- * @param PTree parse tree
+ * @param min_precedence minimum precedence 
+ * @param PTree initialized parse tree
  * @return PTreeNode_t*
  */
 PTreeNode_t *parse_expression(token_list_t *tokens, int min_precedence, PTreeNode_t *PTree)
@@ -86,4 +84,30 @@ PTreeNode_t *parse_expression(token_list_t *tokens, int min_precedence, PTreeNod
         ACTIVE_NEXT;
     }
     return PTree;
+}
+
+PTreeNode_t *parse_expression_with_tree(token_list_t *tokens, int min_precedence, PTreeNode_t *PTree, int *err_code)
+{
+    if (ACTIVE_TYPE == T_Int || ACTIVE_TYPE == T_Exp || ACTIVE_TYPE == T_Float || ACTIVE_TYPE == T_String || ACTIVE_TYPE == T_L_r_par || ACTIVE_TYPE == T_Var_id){
+        if(ACTIVE_TYPE == T_L_r_par){
+            ACTIVE_NEXT;
+            parse_expression_with_tree(tokens, min_precedence, PTree, err_code);
+        }
+        insertLeftPtreeNode(PTree, ACTIVE_TOKEN);
+        ACTIVE_NEXT;
+        while (precedence(ACTIVE_TYPE) >= min_precedence)
+        {
+            token_type_t op = ACTIVE_TYPE;
+            ACTIVE_NEXT;
+            PTreeNode_t *tmp = initPtree();
+            insertRightPtreeNode(PTree, tmp);
+            while (precedence(ACTIVE_TYPE) > precedence(op))
+            {
+                tmp = parse_expression(tokens, precedence(op) + 1, tmp);
+            }
+            insertLeftPtreeNode(tmp, ACTIVE_TOKEN);
+            ACTIVE_NEXT;
+        }
+    }
+    *err_code = EXIT_FAILURE;
 }
